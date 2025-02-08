5e58bf565a2eead6f19bb9d328375d39bca0a8e5
Gurux3 <guruhari86572@gmail.com>
2025-02-08 23:16:22 +0530

    day01 - python commit

--- data_types.py
+++ data_types.py
@@ -1,4 +1,45 @@
+# this is single line comment
+
+"""
+this is multi line
+comment ❤️
+"""
+
+# Python is an general purpose progrmming lang developed by Guido van Rossum
+# started at 1980 and relesed 1.0 in 1991
+
+# premitive data types
+  # int
+  # float
+  # bool
+
+a = 20
+b = 3
+print(a + b)  # 23      --addition
+print(a - b)  # 17      --subraction
+print(a * b)  # 60      --multiplication
+print(a / b)  # 6.666666666666667 --divition
+print(a // b)  # 6     --integer divition
+print(a % b)  # 2      --moduluo operator
+print(a ** b)  # 8000  --exponantial
 
 
-print ("Hello world")
+# boolean datatypes
 
+a  = True
+b = False
+# case sensitive T and F are
+
+
+
+# Chaining comparison operators in Python
+
+a = 100
+b = 10
+c  = a + b
+
+print(a < b < c)
+# false 100 < 10 < 110
+
+# is  is an special operator that checks the 2 variables obj / referance and if match then true else false !!!
+# this are the topics covered today
